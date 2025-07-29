94137e973b7631f1ca0ced33e354fcf3e9954cd9
krish-kolarya <krishnakolarya@gmail.com>
2025-07-29 13:27:20 +0530

    Fixing API KEY

--- src/api/OpenWeatherService.js
+++ src/api/OpenWeatherService.js
@@ -1,17 +1,18 @@
 const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-const WEATHER_API_KEY = 'Your API KEY';
+const WEATHER_API_KEY = process.env.REACT_APP_OPENWEATHER_API_KEY;
 
 const GEO_API_OPTIONS = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': '4f0dcce84bmshac9e329bd55fd14p17ec6fjsnff18c2e61917',
+    'X-RapidAPI-Key': 'b6c7e97f34mshe4a68ba6e1de972p1315dbjsn67deae101421',
     'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
   },
 };
 
 export async function fetchWeatherData(lat, lon) {
+  //console.log('API Key:', WEATHER_API_KEY);
   try {
     let [weatherPromise, forcastPromise] = await Promise.all([
       fetch(
